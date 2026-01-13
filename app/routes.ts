43689b6ac6ab6d19f0d3049aecb4579e6afdb624
Shimoda Keitaro <shimoda.keitaro@raccoon.ne.jp>
2026-01-13 09:29:56 +0900

    fix slide

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-const routeIds = ["1", "2", "3", "4", "5", "5-2", "6", "6-2", "7", "8", "9","10"];
+const routeIds = ["1", "2", "3", "4", "5", "5-2", "6", "6-2", "7", "8", "9", "10"];
 
 export default [
     index("routes/home.tsx"),
